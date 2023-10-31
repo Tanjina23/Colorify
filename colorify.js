018d31b822f5fdf344d07421c069fbaff759bb7e
Syeda Tanjina <syedatanjina111@gmail.com>
2023-10-31 22:22:55 +0600

    Initial commit

---
+++ colorify.js
@@ -0,0 +1,114 @@
+
+        function colorify_1(){
+            let cl = document.getElementById("circle");
+            // let bt = document.getElementById("btn-1");
+            if(cl.style.backgroundColor = "white"){
+                cl.style.backgroundColor = "green";
+           }     
+        }
+
+        function colorify_2(){
+            let cl = document.getElementById("circle");
+            let bt = document.getElementById("btn-1");
+            if(cl.style.backgroundColor = "white"){
+                cl.style.backgroundColor = "red";
+           }     
+        }
+
+        function colorify_3(){
+            let cl = document.getElementById("circle");
+            let bt = document.getElementById("btn-1");
+            if(cl.style.backgroundColor = "white"){
+                cl.style.backgroundColor = "blue";
+           }     
+        }
+
+        function colorify_4(){
+            let cl = document.getElementById("circle");
+            let bt = document.getElementById("btn-1");
+            if(cl.style.backgroundColor = "white"){
+                cl.style.backgroundColor = "yellow";
+           }     
+        }
+        function colorify_5(){
+            let cl = document.getElementById("circle");
+            let bt = document.getElementById("btn-1");
+            if(cl.style.backgroundColor = "white"){
+                cl.style.backgroundColor = "mediumvioletred";
+           }     
+        }
+        function colorify_6(){
+            let cl = document.getElementById("circle");
+            let bt = document.getElementById("btn-1");
+            if(cl.style.backgroundColor = "white"){
+                cl.style.backgroundColor = "maroon";
+           }     
+        }
+        function colorify_7(){
+            let cl = document.getElementById("circle");
+            let bt = document.getElementById("btn-1");
+            if(cl.style.backgroundColor = "white"){
+                cl.style.backgroundColor = "springgreen";
+           }     
+        }
+        function colorify_8(){
+            let cl = document.getElementById("circle");
+            let bt = document.getElementById("btn-1");
+            if(cl.style.backgroundColor = "white"){
+                cl.style.backgroundColor = "seagreen";
+           }     
+        }
+        function colorify_9(){
+            let cl = document.getElementById("circle");
+            let bt = document.getElementById("btn-1");
+            if(cl.style.backgroundColor = "white"){
+                cl.style.backgroundColor = "aqua";
+           }     
+        }
+        function colorify_10(){
+            let cl = document.getElementById("circle");
+            let bt = document.getElementById("btn-1");
+            if(cl.style.backgroundColor = "white"){
+                cl.style.backgroundColor = "crimson";
+           }     
+        }
+
+        function colorify_11(){
+            let cl = document.getElementById("circle");
+            let bt = document.getElementById("btn-1");
+            if(cl.style.backgroundColor = "white"){
+                cl.style.backgroundColor = "tomato";
+           }     
+        }
+
+        function colorify_12(){
+            let cl = document.getElementById("circle");
+            let bt = document.getElementById("btn-1");
+            if(cl.style.backgroundColor = "white"){
+                cl.style.backgroundColor = "thistle";
+           }     
+        }
+
+        function colorify_13(){
+            let cl = document.getElementById("circle");
+            let bt = document.getElementById("btn-1");
+            if(cl.style.backgroundColor = "white"){
+                cl.style.backgroundColor = "salmon";
+           }     
+        }
+        function colorify_14(){
+            let cl = document.getElementById("circle");
+            let bt = document.getElementById("btn-1");
+            if(cl.style.backgroundColor = "white"){
+                cl.style.backgroundColor = "darkblue";
+           }     
+        }
+    
+
+      function Reset(){
+        let cl = document.getElementById("circle");
+        let rt = document.getElementById("rt");
+        if(cl.style.backgroundColor != "white"){
+            cl.style.backgroundColor = "white";
+        }
+      }
